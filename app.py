393691b3ae15c09345ef5ce29e5f5b94f021010f
Kyle Roux <kyle@casepeer.com>
2017-02-06 09:28:20 -0800

    fixs

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ class EventListView(flask_views.MethodView):
     def get(self,cal_id=None):
         if cal_id is None:
             return flask.abort(404)
-        events = load_session().get("https://api.cronofy.com/v1/events?tzid=America/Los_Angeles".format(cal_id)).json()
+        events = load_session().get("https://api.cronofy.com/v1/events?tzid=America/Los_Angeles&include_manages=true".format(cal_id)).json()
         return flask.jsonify(events)
 
 app.add_url_rule('/event/<cal_id>','list_events',view_func=EventListView.as_view('list_events'))
@@ -148,7 +148,6 @@ class IndexView(flask_views.MethodView):
             if REDIRECT_URI is not None:
                 form_args['redirect_uri'] = REDIRECT_URI
             form = CodeForm(**form_args)
-
             args = {'form':form}
             return_response = flask.render_template(template,**args)
         return return_response
